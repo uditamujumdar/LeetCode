ccefa8f945d5b1a723fb1de4ec4b90a328330c64
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2023-10-23 23:05:52 +0530

    Time: 0 ms (100%), Space: 6.8 MB (5.26%) - LeetHub

--- 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.cpp
+++ 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int getMaximumGenerated(int n) {
-        if(n<2)return n;
+        if(n==0)
+        return 0;
+
         vector<int>v(n+1);
-       v[0]=0;
-       v[1]=1;
-        for(int i=1;i<=n;i++)
-        {
-            if(!(i&1))
-            {
-                 v[i]=v[i/2];
+        v[0]=0, v[1]=1;
+
+        for(int i=2;i<=n;i++){
+            if(i%2==0){
+                v[i]=v[i/2];
             }
-            else
-            {
-                v[i]=v[i/2]+v[i/2 +1];
+            else{
+                v[i]=v[i/2]+v[i/2+1];
             }
         }
-        
-        sort(v.begin(),v.end());
-        return v[v.size()-1];
+
+        return *max_element(v.begin(), v.end());
     }
 };
