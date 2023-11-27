6d1258515dd6d86a03387eabde33380e49588cea
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2023-11-27 23:04:38 +0530

    Time: 42 ms (50.00%) | Memory: 7 MB (50.00%) - LeetSync

---
+++ 3210-count-beautiful-substrings-i/count-beautiful-substrings-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int beautifulSubstrings(string s, int k) {
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            int v=0;
+            int c=0;
+            for(int j=i;j<s.size();j++){
+                if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'|| s[j]=='u'){
+                    v++;
+                }
+                else{
+                    c++;
+                }
+                
+                if((v==c)&& (v*c)%k==0){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
