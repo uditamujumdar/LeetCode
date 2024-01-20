d48eac955fd31542f693bd2cc53e5aed78ad06dd
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-01-21 01:23:06 +0530

    Time: 56 ms (97.51%) | Memory: 48.5 MB (5.08%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        int mod=1e9+7;
+        stack<int> s1, s2;
+        vector<long long> left(n), right(n, n);
+
+        // Calculate left boundaries
+        for (int i = 0; i < n; ++i) {
+            while (!s1.empty() && arr[s1.top()] > arr[i]) {
+                s1.pop();
+            }
+            left[i] = s1.empty() ? i + 1 : i - s1.top();
+            s1.push(i);
+        }
+
+        // Calculate right boundaries
+        for (int i = n - 1; i >= 0; --i) {
+            while (!s2.empty() && arr[s2.top()] >= arr[i]) {
+                s2.pop();
+            }
+            right[i] = s2.empty() ? n - i : s2.top() - i;
+            s2.push(i);
+        }
+
+        // Calculate the final sum
+        long long result = 0;
+        for (int i = 0; i < n; ++i) {
+            result = (result + arr[i] * left[i] * right[i]) % mod;
+        }
+        
+        return result;
+    }
+};
