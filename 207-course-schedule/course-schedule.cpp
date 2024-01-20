8808d719e554b3e86c83f8d2215c18425a22cfb1
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-01-20 17:57:44 +0530

    Time: 19 ms (58.62%) | Memory: 17.5 MB (7.24%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prereq) {
+        vector<int>adj[n];
+        for(auto it:prereq){
+            adj[it[0]].push_back(it[1]);
+        }
+
+        vector<int>indeg(n,0);
+
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                indeg[it]++;
+            }
+        }
+
+        vector<int>topo;
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+
+        return topo.size()==n;
+    }
+};
