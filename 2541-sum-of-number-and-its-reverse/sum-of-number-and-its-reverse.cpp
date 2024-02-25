03772f2d8ea2cedaff0de8473cf69d361ab746d2
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-02-25 13:03:50 +0530

    Time: 56 ms (68.48%) | Memory: 7.1 MB (29.89%) - LeetSync

---
+++ 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int num){
+        int rev=0;
+        while(num>0){
+            int dig=num%10;
+            rev=rev*10+dig;
+            num/=10;
+        }
+        return rev;
+    }
+    bool sumOfNumberAndReverse(int num) {
+        for(int i=0;i<=num;i++){
+            if(i+reverse(i)==num){
+                return true;
+            }
+        }
+        return false;
+    }
+};
