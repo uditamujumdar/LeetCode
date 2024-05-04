d6ea36f895114df70bb7e587ff5e3b0075adb599
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-05-05 01:14:18 +0530

    Time: 3 ms (52.40%) | Memory: 10.2 MB (21.24%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void f(int ind, vector<int>&nums, vector<int>&output, vector<vector<int>>&ans, int n){
-        if(ind==n){
-            ans.push_back(output);
+    void solve(vector<int>nums,vector<vector<int>>& ans, int index){
+        if(index>=nums.size()){
+            ans.push_back(nums);
             return;
         }
 
-        for(int i=ind;i<n;i++){
-            swap(nums[i],nums[ind]);
-            output.push_back(nums[ind]);
-            f(ind+1,nums,output,ans,n);
-            output.pop_back();
-            swap(nums[i],nums[ind]);
+        for(int j=index;j<nums.size();j++){
+            swap(nums[index], nums[j]);
+
+            solve(nums, ans, index+1);
+
+            swap(nums[index], nums[j]);
         }
     }
 
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
-        vector<int>output;
-        int n=nums.size();
-        f(0,nums,output,ans,n);
 
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index=0;
+        solve(nums, ans, index);
         return ans;
     }
 };
