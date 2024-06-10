6a57e4652f69f26b42a6216b830f3930db56ed6e
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-06-10 12:50:30 +0530

    Time: 2 ms (48.33%) | Memory: 10.3 MB (10.73%) - LeetSync

---
+++ 1137-height-checker/height-checker.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>v=heights;
+        sort(heights.begin(), heights.end());
+        
+        int ans=0;
+        for(int i=0;i<v.size();i++){
+            if(heights[i]!=v[i]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
