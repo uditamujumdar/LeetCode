8e2a19313cc15fb30e3c96c15426e5952101497a
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-07-25 18:25:36 +0530

    Time: 212 ms (24.98%) | Memory: 152.7 MB (17.71%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -4,20 +4,18 @@ public:
         vector<int>ans;
         int n=nums.size();
         priority_queue<pair<int,int>>pq;
-        for(int i=0;i<k;i++){
-            pq.push({nums[i],i});
-        }
-        ans.push_back(pq.top().first);
+        int i=0, j=0;
 
-        for(int i=k;i<n;i++){
-            pq.push({nums[i],i});
-
-            while(pq.top().second<=i-k){
+        while(j<n){
+            while(j-i<k){
+                pq.push({nums[j], j++});
+            }
+            while(!pq.empty() && j-pq.top().second>k){
                 pq.pop();
             }
             ans.push_back(pq.top().first);
+            i++;
         }
-
         return ans;
     }
 };
