8e02a8cd6b1d8686908f3327048d0259df18f86a
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-08-01 14:54:46 +0530

    Time: 25 ms (93.96%) | Memory: 15.6 MB (17.99%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        vector<int>occ;
-        long long count=0;
-        
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='1'){
-                occ.push_back(i);
-            }
+        long long ans=0;
+        int n=s.size();
+        long long cnt=0;
+
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0')cnt++;
+            else ans+=cnt;
         }
-        
-        int n=s.length()-1;
-        
-        if(occ.size()==0 || occ.size()==n+1)return 0;
-        
-        for(int i=occ.size()-1;i>=0;i--){
-            count+=n-occ[i];
-            n=n-1;
-        }
-        
-        return count;
+        return ans;
     }
 };
