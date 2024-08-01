1f7d44b9e51debab0a1cd5bc4a767f0d59987329
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-08-02 01:06:25 +0530

    Time: 26 ms (84.86%) | Memory: 20.5 MB (30.01%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& mat, vector<vector<int>>&dp){
+    int f(int r, int c,vector<vector<int>>&mat,vector<vector<int>>&vis,vector<vector<int>>&dp){
+        int dr[]={-1,0,1,0};
+        int dc[]={0,1,0,-1};
         int n=mat.size();
         int m=mat[0].size();
-        if(i<0||j<0||i>=n||j>=m){
-            return 0;
+        if(dp[r][c]!=-1){
+            return dp[r][c];
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+        int ans=1;
+        vis[r][c]=1;
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            if(nr>=0 && nc>=0 && nc<m && nr<n && !vis[nr][nc] && mat[nr][nc]>mat[r][c]){
+                ans=max(ans, 1+f(nr,nc,mat,vis,dp));
+            }
         }
-
-        int u=0,d=0,l=0,r=0;
-        if(i-1>=0 && mat[i-1][j]>mat[i][j]){
-            u=1+f(i-1,j,mat,dp);
-        }
-        if(i+1<n && mat[i+1][j]>mat[i][j]){
-            d=1+f(i+1,j,mat,dp);
-        } 
-        if(j-1>=0 && mat[i][j-1]>mat[i][j]){
-            l=1+f(i,j-1,mat,dp);
-        }
-        if(j+1<m && mat[i][j+1]>mat[i][j]){
-            r=1+f(i,j+1,mat,dp);
-        }
-
-        return dp[i][j]=max(u,max(d,max(l,r)));
+        vis[r][c]=0;
+        return dp[r][c]=ans;
     }
 
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int ans=INT_MIN;
+    int longestIncreasingPath(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int ans=-1;
+        vector<vector<int>>vis(n, vector<int>(m, 0));
         vector<vector<int>>dp(n, vector<int>(m, -1));
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                ans=max(ans, 1+f(i,j,matrix,dp) );
+                ans=max(ans, f(i,j,mat,vis,dp));
             }
         }
         return ans;
