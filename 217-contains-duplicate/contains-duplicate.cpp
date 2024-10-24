5d1494d34be8c24b374cf0c7be01afceefc50137
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-10-24 22:56:19 +0530

    Time: 14 ms (99.89%) | Memory: 61.1 MB (81.36%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,8 +3,8 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n=nums.size();
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
                 return true;
             }
         }
