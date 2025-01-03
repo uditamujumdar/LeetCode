f88e466eae420fcdc639e7e37d361a93bab50816
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2025-01-03 16:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 89.2 MB (87.28%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
         int n=nums.size();
-        vector<long long>pref(n);
-        pref[0]=nums[0];
-        for(int i=1;i<n;i++){
-            pref[i]=pref[i-1]+nums[i];
-        }
-
+        long long total=accumulate(nums.begin(), nums.end(),(long long)0);
+        long long sum=0;
         int ans=0;
+
         for(int i=0;i<n-1;i++){
-            long long s1=pref[i];
-            long long s2=pref[n-1]-pref[i];
-            if(s1>=s2){
+            sum+=nums[i];
+            if(sum>=(total-sum)){
                 ans++;
             }
         }
