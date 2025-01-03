dfb21f0c786c83ed34355065067cefb57b7f6d3a
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2025-01-03 16:11:15 +0530

    Time: 69 ms (10.35%) | Memory: 146.6 MB (11.49%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        int n = nums.size();
-        vector<int> diff(n, 0);
-        long long sum = 0l;
-        for(int i=0;i<n;i++) {
-            diff[i] = (nums[i]^k )- nums[i];
-            sum+=nums[i]*1l;
-        } 
-        sort(diff.begin(), diff.end(), greater<int>());
-        for(int i=0;i<n-1;i+=2) {
-            if((diff[i] + diff[i+1])>0) {
-                sum+=(diff[i] + diff[i+1])*1l;
-            }
+    long long f(int i, int c, vector<int>&nums, int k, vector<vector<long long>>&dp){
+        if(i>=nums.size()){
+            return c?INT_MIN:0;
         }
-        return sum;
+        if(dp[i][c]!=-1){
+            return dp[i][c];
+        }
+        long long x=nums[i];
+
+        return dp[i][c]=max(x+f(i+1,c,nums,k,dp), (x^k)+f(i+1,!c,nums,k,dp));
+    }
+
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        int n=nums.size();
+        vector<vector<long long>>dp(n, vector<long long>(2,-1));
+
+        return f(0,0,nums,k,dp);
     }
 };
