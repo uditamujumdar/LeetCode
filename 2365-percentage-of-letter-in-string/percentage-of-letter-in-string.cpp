6b27bc7f414e73dab2b8860aad487abad5d15010
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2025-01-28 18:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (68.03%) - LeetSync

---
+++ 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int n=s.length();
+        int cnt=0;
+
+        for(int i=0;i<n;i++){
+            if(s[i]==letter){
+                cnt++;
+            }
+        }
+
+        int ans=(cnt*100)/n;
+        return ans;
+    }
+};
