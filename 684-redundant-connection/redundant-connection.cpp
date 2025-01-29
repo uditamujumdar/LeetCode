eae0fbb8c283bb7e8883fc139cdd9c580b537aa3
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2025-01-29 16:59:54 +0530

    Time: 3 ms (37.03%) | Memory: 13.2 MB (38.03%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,71 @@
+class DisjointSet{
+    public:
+    vector<int>rank, parent, size;
+    DisjointSet(int n){
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUParent(u); 
+        int ulp_v = findUParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUParent(u); 
+        int ulp_v = findUParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        DisjointSet ds(n);
+        vector<int>ans(2);
+
+        for(auto it: edges){
+            int u=it[0];
+            int v=it[1];
+
+            if(ds.findUParent(u)==ds.findUParent(v)){
+                ans[0]=u;
+                ans[1]=v;
+            }
+            else{
+                ds.unionBySize(u,v);
+            }
+        }
+        return ans;
+    }
+};
