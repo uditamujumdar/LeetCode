c0efbb8c39319e46564b631ca08c250550027d99
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2025-02-05 00:39:33 +0530

    Time: 4 ms (3.19%) | Memory: 27.9 MB (65.22%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
-        int maxi1=INT_MIN, maxi2=INT_MIN, cnt1=0, cnt2=0;
+        int ans1=0;
+        int ans2=0;
         
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]<nums[i+1]){
-                cnt1++;
-                maxi1=max(maxi1, cnt1);
+        for(int i=0;i<nums.size();i++)
+        {
+            int cnt=0;
+            for(int j=i;j<nums.size()-1;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    cnt++;
+                }
+                else
+                {
+                    break;
+                }
             }
-            else{
-                cnt1=0;
-            }
-            if(nums[i]>nums[i+1]){
-                cnt2++;
-                maxi2=max(maxi2, cnt2);
-            }
-            else{
-                cnt2=0;
-            }
+            ans1=max(ans1,cnt+1);
         }
-        if(maxi1==INT_MIN && maxi2==INT_MIN){
-            return 1;
+        for(int i=0;i<nums.size();i++)
+        {
+            int nct=0;
+            for(int j=i;j<nums.size()-1;j++)
+            {
+                if(nums[j]<nums[j+1])
+                {
+                    nct++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            ans2=max(ans2,nct+1);
         }
-        
-        return max(maxi1, maxi2)+1;
+        cout<<ans1<<" "<<ans2<<endl;
+        return max(ans1,ans2);
     }
 };
