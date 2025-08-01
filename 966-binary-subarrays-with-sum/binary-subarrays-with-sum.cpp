efa5d11a6dc4166e658ad9bf1860b01a8da90713
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2025-08-01 15:56:16 +0530

    Time: 2 ms (57.25%) | Memory: 32.6 MB (59.42%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
     int f(vector<int>& nums, int goal){
-        if(goal<0)
-            return 0;
-
+        int n=nums.size();
+        int ans=0;
+        int i=0, j=0;
         int sum=0;
-        int i=0;
-        int j=0;
-        int res=0;
+        if(goal<0){
+            return 0;
+        }
 
-        while(j<nums.size()){
-            //we're iterating over the nums array and 
-            //then adding the current element into the sum
-            sum+=nums[j]; 
-            
-            //if our current sum is greater than our goal
-            //that means we don't need the extra element
-            //so we will shrink our window by subtracting 
-            //the ith elements from sum till sum > goal
+        while(j<n){
+            sum+=nums[j];
+
             while(sum>goal){
                 sum-=nums[i];
                 i++;
             }
-            //We're adding the length of each subarray 
-            //to our result
-            res+=(j-i+1);
-            
+            ans+=(j-i+1);
             j++;
         }
-        
-        return res;
+
+        return ans;
     }
 
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return f(nums, goal)-f(nums,goal-1);
+        return f(nums, goal)-f(nums, goal-1);
     }
 };
