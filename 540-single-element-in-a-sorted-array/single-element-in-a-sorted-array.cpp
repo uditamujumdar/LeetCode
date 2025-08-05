261de28a06a502bfddd425444e925177cf1453e4
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2025-08-06 02:10:03 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.32%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& a) {
-        int n=a.size();
-        if(n==1) return a[0];
-        if(a[0]!=a[1]) return a[0];
-        if(a[n-1]!=a[n-2]) return a[n-1];
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){
+            return nums[0];
+        }
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        if(nums[n-2]!=nums[n-1]){
+            return nums[n-1];
+        }
 
         int i=1, j=n-2;
-
         while(i<=j){
             int mid=i+(j-i)/2;
-
-            if(a[mid]!=a[mid+1] && a[mid]!=a[mid-1]){
-                return a[mid];
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
             }
-
-            if((mid&1) && (a[mid-1]==a[mid]) || (mid%2==0)&& (a[mid]==a[mid+1])){
+            if((mid%2 && nums[mid-1]==nums[mid]) || (mid%2==0 && nums[mid+1]==nums[mid])){
                 i=mid+1;
             }
             else{
