1fefb80dc146dc30b876f7aafb47dfa94fe65d09
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2025-08-06 15:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (92.13%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -8,26 +8,26 @@ public:
         if(nums[0]>nums[1]){
             return 0;
         }
-        if(nums[n-1]>nums[n-2]){
+        if(nums.back()>nums[n-2]){
             return n-1;
         }
 
-        int i=1,j=n-2;
-        while(i<=j){
-            int mid=i+(j-i)/2;
+        int i=1, j=n-2;
+        int ans=0;
 
+        while(j<n){
+            int mid=i+(j-i)/2;
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
                 return mid;
             }
-
-            if(nums[mid]>nums[mid-1]){
+            else if(nums[mid]>nums[mid-1]){
                 i=mid+1;
             }
-
             else{
                 j=mid-1;
             }
         }
+
         return -1;
     }
 };
